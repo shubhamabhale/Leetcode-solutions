854bf75e5015e9f0eebf28d19e279257e1b61ed0
Shubham Abhale <50108956+shubhamabhale@users.noreply.github.com>
2024-02-04 11:40:36 -0800

    Time: 7 ms (96.34%), Space: 46.4 MB (49.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,32 +1,37 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length <= 1) {
-            return intervals;
-        }
-
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
-
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare (int[] o1, int[] o2) {
+                return Integer.compare(o1[0],o2[0]);
+            }
+        });
         List<int[]> merged = new ArrayList<>();
 
         int[] currInterval = intervals[0];
 
-        for (int i = 1; i < intervals.length; i++) {
-            if (currInterval[1] >= intervals[i][0]) {
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(currInterval[1]>=intervals[i][0])
+            {
                 currInterval[1] = Math.max(currInterval[1], intervals[i][1]);
-            } else {
+                currInterval[0] = Math.min(currInterval[0],intervals[i][0]);
+            }
+            else
+            {
                 merged.add(currInterval);
                 currInterval = intervals[i];
             }
         }
-
-        merged.add(currInterval);
-
+         merged.add(currInterval);
         int[][] result = new int[merged.size()][2];
 
-        for (int i = 0; i < merged.size(); i++) {
+        for(int i=0;i<merged.size();i++)
+        {
             result[i] = merged.get(i);
         }
-
         return result;
+
+
     }
 }
