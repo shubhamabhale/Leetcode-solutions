aad5a09978b12014b13aaf15347ad0f9053e7baa
Shubham Abhale <50108956+shubhamabhale@users.noreply.github.com>
2024-02-28 07:09:42 -0800

    Time: 20 ms (54.89%), Space: 44.9 MB (72.31%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.java
+++ 1117-building-h2o/1117-building-h2o.java
@@ -1,23 +1,19 @@
 class H2O {
-    public Semaphore oxygen;
-    public Semaphore hydrogen;
+    private Semaphore oxygen;
+    private Semaphore hydrogen;
     public H2O() {
         oxygen = new Semaphore(0);
         hydrogen = new Semaphore(2);
     }
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-		
-        oxygen.acquire();
-        
+		oxygen.acquire();
         // releaseHydrogen.run() outputs "H". Do not change or remove this line.
         releaseHydrogen.run();
         hydrogen.release();
-        
     }
 
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        
         hydrogen.acquire(2);
         // releaseOxygen.run() outputs "O". Do not change or remove this line.
 		releaseOxygen.run();
